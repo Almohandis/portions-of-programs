0043a9fb3d8b7985553b3afa3228f7bcd4d73028
Almohndis <ur843d@gmail.com>
2022-02-12 09:58:49 +0200

    oop principles c#/

---
+++ oop principles c#/oopPrinciples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace oopPrinciples
+{
+    class MediaProduction
+    {
+        private void SaveFile() { }
+        private void OpenFile() { }
+        private void CreateProject() { }
+        public enum Theme { Black, White, Blue, Purple };
+        Theme theme;
+        protected virtual void EditFrame() { Console.WriteLine("Edit video frame"); }
+        protected virtual void AddFrame(Theme theme) { Console.WriteLine("Add voice frame"); }
+        public void AutoEditFrame()
+        {
+            OpenFile();
+            SaveFile();
+            EditFrame();
+            SaveFile();
+        }
+        public void AutoAddFrame(Theme theme)
+        {
+            OpenFile();
+            CreateProject();
+            SaveFile();
+            AddFrame(theme);
+            SaveFile();
+        }
+
+    }
+    class VideoEditing:MediaProduction
+    {
+        protected override void AddFrame(Theme theme) { Console.WriteLine("Add video frame"); }
+        public void AddVideo() { }
+    }
+    class VoiceEditing : MediaProduction
+    {
+        protected override void EditFrame() { Console.WriteLine("Edit voice frame"); }
+        public void AddVoice() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaProduction m1 = new VideoEditing();
+            MediaProduction m2 = new MediaProduction();
+        }
+    }
+}
