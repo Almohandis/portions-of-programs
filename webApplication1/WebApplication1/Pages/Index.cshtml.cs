3761e9ba53a7173f348bbd5098f75737ec963262
Almohndis <ur843d@gmail.com>
2022-02-12 09:34:56 +0200

    web application 1

---
+++ webApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WebApplication1.ViewModels;
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexViewModel ViewModel { get; set; }
+        public string Value { get; set; }
+       [BindProperty (SupportsGet =true)] public string Name { get; set; }
+        
+        public void OnGet()
+        {
+            Value = "Hello c#".ToUpper();
+        }
+        public string Result { get; set; }
+        [BindProperty ] public string name { get; set; }
+        public void OnPost()
+        {
+            if (name == "abdo")
+                Result = "true";
+            else
+                Result = "False";
+            OnGet();
+        }
+    }
+}
